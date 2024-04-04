37c02846aa43891bc01c7db1ffe037f575947dac
Escobar28 <eq17001@ues.edu.sv>
2024-04-04 13:43:04 -0600

    Clase de rutas realizada

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/test', function(){
+//     return "Página de Prueba";
+// });
+
+Route::get('/test', function(){
+    return view('test');
+})->name('test');
+
+// Route::get('/crud', function(){
+//     return view('crud/index');
+// });
+
+// Route::get('/crud', function(){
+//     $age = 27;
+//     $name = 'Francisco';
+//     $data = ['name'=> $name, 'age'=> $age];
+//     return view('crud/index', $data);
+// });
+
+Route::get('/crud', function(){
+    $age = 27;
+    $name = 'Francisco';
+    return view('crud/index', ['name'=> $name, 'age'=> $age]);
+})->name('crud');
