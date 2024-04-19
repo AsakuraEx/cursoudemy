8d0c839f9593e63c553e31c6a2fb9c5046e44917
Escobar28 <eq17001@ues.edu.sv>
2024-04-19 14:44:11 -0600

    Laravel Breeze instalado

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,32 @@
 
 use App\Http\Controllers\Dashboard\CategoryController;
 use App\Http\Controllers\Dashboard\PostController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('post', PostController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('category', CategoryController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Route::resource('post', PostController::class);
+// Route::resource('category', CategoryController::class);
+
+Route::group(['prefix' => 'dashboard'], function(){
+    
+    Route::resource('post', PostController::class);
+    Route::resource('category', CategoryController::class)->except('show');
+
+});
 
 // Route::get('/contact', function() {
 //     $users = ['Francisco Escobar', 'Diego Escobar', 'Sara Mancia'];
@@ -26,3 +43,5 @@ Route::resource('category', CategoryController::class);
 
 // Route::get('/test',[PrimerControlador::class, 'index']);
 // Route::resource('/test', PrimerControlador::class);
+
+require __DIR__.'/auth.php';
